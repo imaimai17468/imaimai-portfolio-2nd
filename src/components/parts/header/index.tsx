486c81710ac9c1fc1343772ae55dddbc3eb42cf3
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-18 02:36:03 +0900

    Update Header component styles for improved layout and aesthetics

--- src/components/parts/header/index.tsx
+++ src/components/parts/header/index.tsx
@@ -2,8 +2,8 @@ import type React from "react";
 
 export const Header: React.FC = () => {
   return (
-    <header>
-      <h1>imaimai portfolio</h1>
+    <header className="w-fit p-2 rotate-90 fixed top-16 -left-12">
+      <h1 className="text-zinc-500">imaimai portfolio</h1>
     </header>
   );
 };
