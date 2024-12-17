3bd21037631257285383b3b725f162785b38310f
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-18 02:03:29 +0900

    Add Header component and integrate it into MainLayout; update Tailwind config with Raleway font

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,7 +1,14 @@
+import { Header } from "@/components/parts/header";
+
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
 };
---
+++ src/components/parts/header/index.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>imaimai portfolio</h1>
+    </header>
+  );
+};
