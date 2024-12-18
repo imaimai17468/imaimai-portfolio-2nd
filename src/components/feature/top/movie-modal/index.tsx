bbe8a0452d59425babdc5864f0b6a9ef3c5ef3cf
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-18 18:15:32 +0900

    Add MovieModal component for displaying YouTube videos in a dialog; includes title and description props for enhanced user interaction.

---
+++ src/components/feature/top/movie-modal/index.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { InfoIcon } from "lucide-react";
+
+type Props = {
+  title?: string;
+  description?: string;
+  src: string;
+};
+
+export const MovieModal: React.FC<Props> = ({ title, description, src }) => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="ghost" size="icon">
+          <InfoIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          {title && <DialogTitle>{title}</DialogTitle>}
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogHeader>
+        <iframe
+          width="100%"
+          height="315"
+          src={src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
