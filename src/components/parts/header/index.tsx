6703c37c16d301c70a6d397164361980ddc8b438
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-18 17:30:19 +0900

    Enhance Header component with GitHub link and layout adjustments

--- src/components/parts/header/index.tsx
+++ src/components/parts/header/index.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import type React from "react";
 
 export const Header: React.FC = () => {
   return (
-    <header className="w-fit p-2 rotate-90 fixed top-16 -left-12">
-      <h1 className="text-zinc-500">imaimai portfolio</h1>
+    <header className="w-fit p-2 rotate-90 fixed top-28 -left-24 flex gap-4 text-zinc-500">
+      <p>imaimai portfolio</p>
+      <p>-</p>
+      <Link
+        href="https://github.com/imaimai17468/imaimai-portfolio-2nd"
+        className="hover:text-zinc-300 transition-colors duration-300"
+      >
+        <p>GitHub</p>
+      </Link>
     </header>
   );
 };
