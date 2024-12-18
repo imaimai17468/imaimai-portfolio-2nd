b15a6eff7babf9a51687e09a6769be4a5c0872e8
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-19 02:31:46 +0900

    Integrate BackgroundBeamsWithCollision component into MainLayout for enhanced visual effects; adds animated background with collision detection to improve user experience.

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/parts/header";
 import { ScrollCounts } from "@/components/parts/scroll-counts";
+import { BackgroundBeamsWithCollision } from "../ui/background-beams-with-collision";
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -8,6 +9,9 @@ type MainLayoutProps = {
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <div>
+      <BackgroundBeamsWithCollision className="h-screen md:h-screen fixed top-0 left-0 w-full -z-10 dark:to-neutral-900">
+        <div />
+      </BackgroundBeamsWithCollision>
       <Header />
       <ScrollCounts />
       <main>{children}</main>
