c46c542653affa82488e00675c7c67e78576fb94
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-22 01:42:55 +0900

    Enhance Top component styling and text size; update layout for improved visual appeal. Adjusts the Top component's structure by modifying the container's padding and height, and increases the main heading size for better emphasis. This change aims to improve user experience and aesthetics.

--- src/components/feature/top/index.tsx
+++ src/components/feature/top/index.tsx
@@ -1,9 +1,9 @@
 export const Top: React.FC = () => {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center h-screen">
+    <div className="p-2 h-screen">
+      <div className="flex flex-col items-center h-full justify-center border border-zinc-200 rounded-lg">
         <div className="w-fit">
-          <h1 className="text-6xl font-raleway font-black">CREATOR's HIGH</h1>
+          <h1 className="text-9xl font-raleway font-black">CREATOR's HIGH</h1>
           <p className="text-end">－ 開発でキマれ</p>
         </div>
       </div>
