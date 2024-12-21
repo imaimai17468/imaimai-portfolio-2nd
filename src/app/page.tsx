352e51cce212690b5755f99aab111c5851189bf1
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-22 01:17:02 +0900

    Refactor Home component to remove About section; simplifies the layout by displaying only the Top component for a cleaner presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { About } from "@/components/feature/about";
 import { Top } from "@/components/feature/top";
 
 export default function Home() {
-  return (
-    <>
-      <Top />
-      <About />
-    </>
-  );
+  return <Top />;
 }
