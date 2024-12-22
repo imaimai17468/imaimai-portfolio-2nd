5a6a6e428fd969b71c8b52c0b94780863663227a
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-22 13:25:22 +0900

    Refactor About component; integrate Profile and History components for improved structure and content. This update replaces the previous static content with dynamic components, enhancing the user experience by providing a more organized and visually appealing layout. Additionally, the AboutLayout now includes a Menu component for better navigation.

--- src/components/feature/about/layout/index.tsx
+++ src/components/feature/about/layout/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/parts/header";
+import { Menu } from "@/components/parts/menu";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 
 type AboutLayoutProps = {
@@ -9,6 +10,7 @@ export const AboutLayout: React.FC<AboutLayoutProps> = ({ children }) => {
   return (
     <div>
       <Header />
+      <Menu />
       <BackgroundBeamsWithCollision className="h-screen md:h-screen fixed top-0 left-0 w-full -z-10 dark:to-neutral-900">
         <div />
       </BackgroundBeamsWithCollision>
---
+++ src/components/feature/about/profile/index.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { FlipWords } from "@/components/ui/flip-words";
+import { MonitorIcon, Tickets, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export const Profile: React.FC = () => {
+  return (
+    <div className="max-w-3xl mx-auto font-light h-screen flex justify-center flex-col gap-16" aria-label="私について">
+      <div className="w-full flex justify-between items-center">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <div className="text-6xl font-black">
+              <FlipWords words={["いまいまい", "imaimai17468", "Toshiki Imai"]} className="px-0" />
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <Button variant="ghost" size="icon" asChild>
+              <Link href="https://github.com/imaimai17468">
+                <Image src="/images/github-mark-white.svg" alt="いまいまいのGitHub" width={20} height={20} />
+              </Link>
+            </Button>
+            <Button variant="ghost" size="icon" asChild>
+              <Link href="https://x.com/imaimai17468">
+                <Image src="/images/x-logo-white.png" alt="いまいまいのX（旧Twitter）" width={20} height={20} />
+              </Link>
+            </Button>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h3 className="text-lg mt-1">株式会社ゆめみ</h3>
+            <ul className="flex flex-col gap-2 pl-4 border-l-2 border-zinc-500 text-sm">
+              <li className="flex gap-2 items-center">
+                <MonitorIcon className="w-4 h-4" />
+                <p>フロントエンドエンジニア</p>
+              </li>
+              <li className="flex gap-2 items-center">
+                <UserIcon className="w-4 h-4" />
+                <p>リクルーター</p>
+              </li>
+              <li className="flex gap-2 items-center">
+                <Tickets className="w-4 h-4" />
+                <p>技育プロジェクト担当</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <Image
+          className="rounded-full w-64 h-64"
+          src="/images/frog.jpeg"
+          alt="いまいまいのプロフィール画像。カエルの上にカタツムリが乗っている。"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  );
+};
