26884509d0428699329c64df79afd8ae59a23154
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-22 13:40:32 +0900

    Update Menu component positioning; change from absolute to fixed

--- src/components/parts/menu/index.tsx
+++ src/components/parts/menu/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Menu: React.FC = () => {
   return (
-    <ul className="flex flex-col gap-4 justify-center absolute bottom-8 left-8 text-lg">
+    <ul className="flex flex-col gap-4 justify-center fixed bottom-8 left-8 text-lg">
       <li className="border-b border-zinc-200 hover:bg-zinc-200 hover:text-zinc-800 transition-all duration-300 px-2">
         <Link href="/">
           <p className="font-black">TOP</p>
