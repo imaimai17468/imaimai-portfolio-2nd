c527702297f89e2e70e4d8f60df6114cde82f446
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-25 02:30:04 +0900

    Refactor Backbone component; extract Motto into a new component for improved structure and maintainability. The Motto component includes the original title and MovieModal, enhancing code organization.

---
+++ src/components/feature/backbone/motto/index.tsx
@@ -0,0 +1,21 @@
+import { MovieModal } from "@/components/feature/backbone/movie-modal";
+
+export const Motto: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-28 items-center justify-center h-screen">
+      <h1 className="text-9xl font-nakamori">
+        <span className="transform scale-150 translate-y-2 translate-x-4 inline-block">筆</span>
+        <span className="transform scale-140 translate-y-8 translate-x-8 inline-block">先</span>
+        <span className="transform scale-100 translate-y-8 translate-x-4 inline-block">の</span>
+        <span className="transform scale-150 translate-y-6 translate-x-4 inline-block">黒</span>
+        <span className="transform scale-170 translate-y-8 translate-x-4 inline-block">は</span>
+        <br />
+        <span className="transform scale-150 translate-y-16 translate-x-16 inline-block">乾</span>
+        <span className="transform scale-140 translate-y-20 translate-x-20 inline-block">か</span>
+        <span className="transform scale-160 translate-y-20 translate-x-16 inline-block">す</span>
+        <span className="transform scale-125 translate-y-16 translate-x-16 inline-block">な</span>
+      </h1>
+      <MovieModal description="Respect" src="https://www.youtube.com/embed/62_EvhRoj2g?si=1bOmyya8zY_fb9q6" />
+    </div>
+  );
+};
