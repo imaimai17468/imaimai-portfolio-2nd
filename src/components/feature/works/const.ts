965b3ac7885c5f2c81f629adb671d23e4ef6f998
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-31 03:34:12 +0900

    Add new works and assets for portfolio showcase
    - Introduced multiple new image and video files to the portfolio section, enhancing visual content.
    - Created a new TypeScript file defining the 'Work' type and an array of works, including titles, video texts, and background paths for each entry.
    - Updated the portfolio structure to include new works such as "Akeome," "AtCoder Team Battle," and "Contrast Color Palette," among others.

---
+++ src/components/feature/works/const.ts
@@ -0,0 +1,81 @@
+export type Work = {
+  title: string;
+  videoText: string;
+  background: string;
+  path: string;
+};
+
+export const WORKS: Work[] = [
+  {
+    title: "imaimai portfolio 2nd",
+    videoText: "imaimai\nportfolio\n2nd",
+    background: "/works/imaimai_portfolio_2nd.mov",
+    path: "/",
+  },
+  {
+    title: "imaimai portfolio",
+    videoText: "imaimai\nportfolio",
+    background: "/works/imaimai_portfolio.mov",
+    path: "/",
+  },
+  {
+    title: "NUTMEG Seeds",
+    videoText: "NUTMEG\nSeeds",
+    background: "/works/nutmeg_seeds.png",
+    path: "/",
+  },
+  {
+    title: "FinanSu",
+    videoText: "FinanSu",
+    background: "/works/finansu.png",
+    path: "/",
+  },
+  {
+    title: "Group Manager 2",
+    videoText: "Group\nManager\n2",
+    background: "/works/group_manager_2.png",
+    path: "/",
+  },
+  {
+    title: "Tracking Parking",
+    videoText: "Tracking\nParking",
+    background: "/works/tracking_parking.png",
+    path: "/",
+  },
+  {
+    title: "TS Audio Visualizer",
+    videoText: "TS Audio\nVisualizer",
+    background: "/works/ts_audio_visualizer.png",
+    path: "/",
+  },
+  {
+    title: "AtCoder Team Battle",
+    videoText: "AtCoder\nTeam\nBattle",
+    background: "/works/atcoder_team_battle.png",
+    path: "/",
+  },
+  {
+    title: "Akeome",
+    videoText: "Akeome",
+    background: "/works/akeome.png",
+    path: "/",
+  },
+  {
+    title: "Contrast Color palette",
+    videoText: "Contrast\nColor\npalette",
+    background: "/works/contrast_color_palette.png",
+    path: "/",
+  },
+  {
+    title: "Silk Weave",
+    videoText: "Silk\nWeave",
+    background: "/works/silk_weave.png",
+    path: "/",
+  },
+  {
+    title: "Tech Tips",
+    videoText: "Tech\nTips",
+    background: "/works/tech_tips.png",
+    path: "/",
+  },
+];
